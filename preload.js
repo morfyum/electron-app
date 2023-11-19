a2988ad23d6033b3cdb51bde9843a59674bb313f
Morfyum <morfyum@gmail.com>
2023-11-19 23:25:25 +0100

    New theme variables like renderer.js

--- preload.js
+++ preload.js
@@ -1,11 +1,23 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
+// Példa változó definiálása és átadása az index.html-ben
+/*
+const currentSystemTheme = 'Ez egy példa változó érték';
+
+contextBridge.exposeInMainWorld('myAPI', {
+    getCustomVariable: () => currentSystemTheme
+});
+*/
+
 
 // Injektálás a render.js-be
 window.addEventListener('DOMContentLoaded', () => {
+	
 	const script = document.createElement('script');
 	script.src = './render.js';
 	//document.head.appendChild(script);
+
+
 });
 
 // contextBridge segítségével engedélyezzük a biztonságos kommunikációt a main és render folyamatok között
@@ -22,7 +34,7 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', eve
 	theme = localStorage.getItem("theme");
 });
 
-
+/*
 window.addEventListener('DOMContentLoaded', () => {	
 	const replaceText = (selector, text) => {
 		const element = document.getElementById(selector)
@@ -32,13 +44,55 @@ window.addEventListener('DOMContentLoaded', () => {
 	for (const dependency of ['chrome', 'node', 'electron']) {
 		replaceText(`${dependency}-version`, process.versions[dependency])
 	}
-})
+})*/
 
 /*
+const chrome_versino = process.versions[chrome]
+console.log("CHROME_VERSIPN: [", chrome_versino, "]")
+
+
+window.addEventListener('DOMContentLoaded')
+*/
+
+
 contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
+  //chrome: () => process.versions.chrome,
+  //electron: () => process.versions.electron,
   // we can also expose variables, not just functions
 })
-*/
+
+
+// Saját változó definiálása és átadása a renderelő folyamatnak
+
+
+const customVariable = 'Ez egy példa saját változó érték';
+
+// Saját adatok biztonságos átadása a renderelő folyamatnak
+contextBridge.exposeInMainWorld('myCustomData', {
+    getCustomVariable: () => customVariable
+});
+
+
+let currentSystemTheme
+
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+	currentSystemTheme = 'dark';
+	console.log("Dark?", currentSystemTheme)
+} else {
+	currentSystemTheme = 'light';
+	console.log("Light?", currentSystemTheme)
+}
+
+//currentSystemTheme = "dark"
+
+/*
+	window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+		var theme = event.matches ? "dark" : "light";
+	});*/
+
+// Saját adatok biztonságos átadása a renderelő folyamatnak
+contextBridge.exposeInMainWorld('currentThemeData', {
+    getCustomVariable: () => currentSystemTheme
+});

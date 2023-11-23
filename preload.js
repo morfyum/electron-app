58956697da401e237c03e11c100185df4e9144de
Morfyum <morfyum@gmail.com>
2023-11-23 16:00:32 +0100

    add singe DOM event listener

--- preload.js
+++ preload.js
@@ -17,8 +17,8 @@ contextBridge.exposeInMainWorld('electron', {
 });
 
 
-/*
-window.addEventListener('DOMContentLoaded', () => {	
+
+window.addEventListener('DOMContentLoaded', () => {
 	const replaceText = (selector, text) => {
 		const element = document.getElementById(selector)
 		if (element) element.innerText = text
@@ -27,15 +27,24 @@ window.addEventListener('DOMContentLoaded', () => {
 	for (const dependency of ['chrome', 'node', 'electron']) {
 		replaceText(`${dependency}-version`, process.versions[dependency])
 	}
-})*/
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+	const replaceText = (selector, text) => {
+		const element = document.getElementById(selector)
+		if (element) element.innerText = text
+	}
+	replaceText(`current-platform`, process.platform)
+})
 
 
+/*
 contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   //chrome: () => process.versions.chrome,
   //electron: () => process.versions.electron,
   // we can also expose variables, not just functions
-})
+})*/
 
 
 // Saját változó definiálása és átadása a renderelő folyamatnak
